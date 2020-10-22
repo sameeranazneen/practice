8f04d84f9cbcb9b033c0c705cbe28d7c8858e637
sameeranazneen <71516661+sameeranazneen@users.noreply.github.com>
2020-10-22 00:02:09 -0400

    Add files via upload

---
+++ Triangle2/TriangleSolver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Triangle1
+{
+    public static class TriangleSolver
+    {
+        public static string Analyze(int x, int y, int z)
+        {
+            string triangleFormed = "";
+
+            if ((x + y > z || y + z > x || x + z > y) && (x > 0 && y > 0 && z > 0))
+            {
+                Console.WriteLine("The given sides form a triangle");
+                if (x == y && y == z)
+                {
+                    triangleFormed = "forms Equilateral Triangle";
+                }
+                else if (x == y || x == z || y == z)
+                {
+                    triangleFormed = "forms Isosceles Triangle";
+                }
+                else
+                {
+                    triangleFormed = "forms Scalene Triangle";
+                }
+
+                return triangleFormed;
+            }
+            else
+            {
+                return "does not form a triangle";
+            }
+        }
+    }
+}
